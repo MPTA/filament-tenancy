3255788a7fb935fa0864ed21be01ec96a96ddd86
Farhad <masihparvaz@gmail.com>
2025-09-04 15:33:09 +0800

    fix some issues

--- config/tenancy.php
+++ config/tenancy.php
@@ -47,8 +47,8 @@ return [
          * Tenant database names are created like this:
          * prefix + tenant_id + suffix.
          */
-        'prefix' => 'tomatophp_',
-        'suffix' => '_db',
+        'prefix' => 'tenant_',
+        'suffix' => '',
 
         /**
          * TenantDatabaseManagers are classes that handle the creation & deletion of tenant databases.
