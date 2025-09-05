c456b3a9260e790251f89ad1674338ef22ee2217
Farhad <masihparvaz@gmail.com>
2025-09-05 11:33:17 +0800

    install translatable

--- app/Providers/Filament/TenantAdminPanelProvider.php
+++ app/Providers/Filament/TenantAdminPanelProvider.php
@@ -18,6 +18,7 @@ use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
+use LaraZeus\SpatieTranslatable\SpatieTranslatablePlugin;
 use TomatoPHP\FilamentTenancy\FilamentTenancyAppPlugin;
 
 class TenantAdminPanelProvider extends PanelProvider
@@ -54,6 +55,7 @@ class TenantAdminPanelProvider extends PanelProvider
             ])
             // ->authGuard('web')
             ->plugin(FilamentTenancyAppPlugin::make())
+            ->plugin(SpatieTranslatablePlugin::make()->defaultLocales(['en', 'zh']))
             ->authMiddleware([
                 Authenticate::class,
             ]);
