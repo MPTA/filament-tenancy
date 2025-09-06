0f6b76411761ab689aaff1783600e2ae0668d7ee
Farhad <masihparvaz@gmail.com>
2025-09-06 13:09:57 +0800

    exchnage rate fix

--- app/Models/Tenants/ExchangeRate.php
+++ app/Models/Tenants/ExchangeRate.php
@@ -2,6 +2,7 @@
 
 namespace App\Models\Tenants;
 
+use App\Models\Base\Currency;
 use Illuminate\Database\Eloquent\Model;
 use Spatie\Translatable\HasTranslations;
 
@@ -10,5 +11,13 @@ class ExchangeRate extends Model
     use HasTranslations;
     
     protected $translatable = ['name'];
-    protected $fillable = ['name', 'code', 'symbol'];
+    protected $fillable = ['from_currency_id', 'to_currency_id', 'rate'];
+
+    public function fromCurrency(){
+        return $this->belongsTo(Currency::class, 'from_currency_id');
+    }
+
+    public function toCurrency(){
+        return $this->belongsTo(Currency::class, 'to_currency_id');
+    }
 }
