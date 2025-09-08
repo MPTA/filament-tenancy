e2beae3f61d854a1005fedc8e9457f8c40964e1f
Farhad <masihparvaz@gmail.com>
2025-09-08 09:40:18 +0800

    ش

--- app/Models/Tenants/TenantUser.php
+++ app/Models/Tenants/TenantUser.php
@@ -7,7 +7,28 @@ use Stancl\Tenancy\Database\Concerns\HasDatabase;
 
 class TenantUser extends Model
 {
+    use HasDatabase;
+    
     protected $table = 'users';
 
-    protected $fillable = ['name', 'email', 'password', 'tenant_id'];
+    protected $fillable = [
+        'name', 
+        'email', 
+        'password', 
+        'email_verified_at',
+        'remember_token'
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
+    }
 }
