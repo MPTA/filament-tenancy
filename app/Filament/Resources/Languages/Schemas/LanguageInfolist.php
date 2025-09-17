ef34e6a979b7ebe342454ccc089cac8fe0cb2b43
Farhad <masihparvaz@gmail.com>
2025-09-17 19:08:38 +0800

    Language

---
+++ app/Filament/Resources/Languages/Schemas/LanguageInfolist.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Filament\Resources\Languages\Schemas;
+
+use Filament\Infolists\Components\TextEntry;
+use Filament\Schemas\Schema;
+
+class LanguageInfolist
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextEntry::make('id')
+                    ->label('ID'),
+                TextEntry::make('code'),
+                TextEntry::make('name'),
+                TextEntry::make('native_name'),
+                TextEntry::make('created_at')
+                    ->dateTime()
+                    ->placeholder('-'),
+                TextEntry::make('updated_at')
+                    ->dateTime()
+                    ->placeholder('-'),
+            ]);
+    }
+}
