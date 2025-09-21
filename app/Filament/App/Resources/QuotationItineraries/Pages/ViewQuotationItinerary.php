bdab488a4b09664cea370664a663afc819829ab5
Farhad <masihparvaz@gmail.com>
2025-09-21 15:35:46 +0800

    یخدث

--- app/Filament/App/Resources/QuotationItineraries/Pages/ViewQuotationItinerary.php
+++ app/Filament/App/Resources/QuotationItineraries/Pages/ViewQuotationItinerary.php
@@ -13,7 +13,7 @@ class ViewQuotationItinerary extends ViewRecord
     protected function getHeaderActions(): array
     {
         return [
-            EditAction::make(),
+            // EditAction::make(),
         ];
     }
 }
