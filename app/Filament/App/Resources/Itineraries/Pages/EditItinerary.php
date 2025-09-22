d64837f813503842ab7b1154dd8eefdea14cc377
Farhad <masihparvaz@gmail.com>
2025-09-22 14:26:14 +0800

    fix

--- app/Filament/App/Resources/Itineraries/Pages/EditItinerary.php
+++ app/Filament/App/Resources/Itineraries/Pages/EditItinerary.php
@@ -3,6 +3,8 @@
 namespace App\Filament\App\Resources\Itineraries\Pages;
 
 use App\Filament\App\Resources\Itineraries\ItineraryResource;
+use App\Filament\App\Resources\QuotationItineraries\QuotationItineraryResource;
+use App\Models\Tenants\QuotationItinerary;
 use Filament\Actions\DeleteAction;
 use Filament\Actions\ViewAction;
 use Filament\Resources\Pages\EditRecord;
@@ -17,5 +19,23 @@ class EditItinerary extends EditRecord
             ViewAction::make(),
             DeleteAction::make(),
         ];
+    }
+
+    protected function getCancelFormAction(): \Filament\Actions\Action
+    {
+        return parent::getCancelFormAction()
+            ->url(function () {
+                // Check if this itinerary belongs to a quotation
+                if ($this->record->itineraryable_type === QuotationItinerary::class) {
+                    $quotationItinerary = $this->record->itineraryable;
+                    if ($quotationItinerary?->quotation) {
+                        // Redirect to the quotation edit page
+                        return QuotationItineraryResource::getUrl('view', ['record' => $quotationItinerary]);
+                    }
+                }
+                
+                // Default redirect to itinerary list
+                return static::getResource()::getUrl('index');
+            });
     }
 }
