e526c4705bbdde02a5c5b09fdbdaa971bd0eff8c
Farhad <masihparvaz@gmail.com>
2025-10-22 15:40:20 +0800

    fix otp register again for delply

--- resources/views/vendor/filament-tenancy/livewire/register-otp.blade.php
+++ resources/views/vendor/filament-tenancy/livewire/register-otp.blade.php
@@ -1,19 +1,19 @@
 <div class="fi-simple-page">
     <section class="grid auto-cols-fr gap-y-6">
-        <x-filament::header.simple
-            :heading="$this->getHeading()"
-            :logo="true"
-        >
-            <x-slot name="subheading">
+        <div class="text-center">
+            <h2 class="text-xl font-bold tracking-tight">
+                {{ $this->getHeading() }}
+            </h2>
+            <p class="mt-2 text-sm text-gray-600 dark:text-gray-400">
                 Please check our <x-filament::link target="_blank" href="https://discord.gg/vKV9U7gD3c">discord server</x-filament::link> and check <b>#otp</b> channel for the OTP.
-            </x-slot>
-        </x-filament::header.simple>
+            </p>
+        </div>
 
-        <x-filament::form wire:submit.prevent="authenticate">
+        <form wire:submit.prevent="authenticate">
             {{ $this->form }}
 
             {{ $this->submitAction }}
-        </x-filament::form>
+        </form>
 
         <div class="text-center">
             <span class="text-gray-400">Don't get the code? please {{ $this->getResendAction }}</span>
