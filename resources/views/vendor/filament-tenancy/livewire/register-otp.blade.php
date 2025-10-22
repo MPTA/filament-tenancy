2caaab855c44e9ff6a821b154ba43452746e14cd
Farhad <masihparvaz@gmail.com>
2025-10-22 15:34:31 +0800

    fix register otp blade becuase deploy

--- resources/views/vendor/filament-tenancy/livewire/register-otp.blade.php
+++ resources/views/vendor/filament-tenancy/livewire/register-otp.blade.php
@@ -1,19 +1,19 @@
 <div class="fi-simple-page">
     <section class="grid auto-cols-fr gap-y-6">
-        <x-filament-panels::header.simple
+        <x-filament::header.simple
             :heading="$this->getHeading()"
             :logo="true"
         >
             <x-slot name="subheading">
                 Please check our <x-filament::link target="_blank" href="https://discord.gg/vKV9U7gD3c">discord server</x-filament::link> and check <b>#otp</b> channel for the OTP.
             </x-slot>
-        </x-filament-panels::header.simple>
+        </x-filament::header.simple>
 
-        <x-filament-panels::form wire:submit.prevent="authenticate">
+        <x-filament::form wire:submit.prevent="authenticate">
             {{ $this->form }}
 
             {{ $this->submitAction }}
-        </x-filament-panels::form>
+        </x-filament::form>
 
         <div class="text-center">
             <span class="text-gray-400">Don't get the code? please {{ $this->getResendAction }}</span>
